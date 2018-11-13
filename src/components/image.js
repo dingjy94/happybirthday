dd2226b9a11c0836b40778a93e520ac9950dfa80
JINGYIDING <dingjy94@gmail.com>
2018-11-12 23:43:55 -0800

    img

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
+import React from 'react';
+
+import './image.css';
+
+//images
+import first from '../images/1.jpeg';
+import second from '../images/2.jpeg';
+import third from '../images/3.jpeg';
+import fourth from '../images/4.jpeg';
+import fifth from '../images/5.jpg';
+import sixth from '../images/6.jpg';
+import seventh from '../images/7.jpeg';
+import ninth from '../images/9.jpeg';
+import tenth from '../images/10.jpg';
+import eleventh from '../images/11.jpg';
+import tweleveth from '../images/12.jpg';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -14,19 +27,19 @@ import Img from 'gatsby-image'
  */
 
 const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
-)
-export default Image
+  <div id="container">
+    <img class="pic pic1" src={first} />
+    <img class="pic pic2" src={second} />
+    <img class="pic pic3" src={third} />
+    <img class="pic pic4" src={fourth} />
+    <img class="pic pic5" src={fifth} />
+    <img class="pic pic6" src={sixth} />
+    <img class="pic pic7" src={seventh} />
+    <img class="pic pic9" src={ninth} />
+    <img class="pic pic10" src={tenth} />
+    <img class="pic pic11" src={eleventh} />
+    <img class="pic pic12" src={tweleveth} />
+  </div>
+);
+
+ export default Image;
